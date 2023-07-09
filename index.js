3e5805efe53a0ee82b974e6ddd18a9c6078625e8
urvashi1210 <urvashishukla1210@gmail.com>
2023-07-09 09:13:54 +0530

    model defined

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const app=express();
 const dotenv=require('dotenv').config();
-const dbConnect=require("./config/dbConnect.js")
+const dbConnect=require("./config/dbConnect.js");
+const authRouter=require('./routes/authRoute')
 const PORT=process.env.PORT||4000;
+
 dbConnect();
-app.use("/",(req,res)=>{
-    res.send(`Hello from server side`);
-})
+
+
+app.use('/api/user',authRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server is running at PORT ${PORT}`);
