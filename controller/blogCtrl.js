7b1fea4baf3100eb8af1b7625bd9607d481909ec
urvashi1210 <urvashishukla1210@gmail.com>
2023-07-16 21:50:23 +0530

    blog model defined

---
+++ controller/blogCtrl.js
@@ -0,0 +1,9 @@
+const Blog=require("../models/blogModel")
+const User=require("../models/userModel");
+const asyncHandler=require("express-async-handler");
+const validateMongoDbId=require("../utils/validateMongodbId");
+
+exports.createBlog=asyncHandler(async(req,res)=>{
+
+});
+
