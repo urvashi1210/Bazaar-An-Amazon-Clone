59681a84cbb00c46bfe67dab91aaed3d35ceb005
urvashi1210 <urvashishukla1210@gmail.com>
2023-07-24 23:31:23 +0530

    Styled footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,121 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsInstagram, BsYoutube, BsGithub } from "react-icons/bs";
+import newsletter from "../images/newsletter.png"
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img src={newsletter}alt="newsletter" />
+                <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div class="input-group">
+                <input
+                  type="text"
+                  className="form-control py-1 "
+                  placeholder="Your email address"
+                  aria-label="Your email address"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="mb-4 text-white">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  Hno: 344 Near White Villa,
+                  <br /> Mumbai <br />
+                  PinCode:116748
+                </address>
+                <a
+                  href="tel : +917684307576"
+                  className="mt-3 text-white mb-1 d-block"
+                >
+                  +917684307576
+                </a>
+                <a
+                  href="mailto:usam12@gmail.com"
+                  className="mt-2 text-white mb-0 d-block"
+                >
+                  usam12@gmail.com
+                </a>
+                <div className="social-icons d-flex align-items-center gap-30 mt-4">
+                  <a href="#" className="text-white">
+                    <BsLinkedin className="fs-4" />
+                  </a>
+                  <a href="#" className="text-white">
+                    <BsInstagram className="fs-4" />
+                  </a>
+                  <a href="#" className="text-white">
+                    <BsGithub className="fs-4" />
+                  </a>
+                  <a href="#" className="text-white">
+                    <BsYoutube className="fs-4" />
+                  </a>
+                  <a href="#"></a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="mb-4 text-white">Information</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link to='privacy-policy' className="text-white mb-1 py-2">Privacy Policy</Link>
+                <Link to='refund-policy' className="text-white mb-1 py-2">Refund Policy</Link>
+                <Link to='shipping-policy' className="text-white mb-1 py-2">Shipping Policy</Link>
+                <Link to='terms-and-conditions' className="text-white mb-1 py-2">
+                  Terms and Conditions
+                </Link>
+                <Link to='/blogs' className="text-white mb-1 py-2">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="mb-4 text-white">Account</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white mb-1 py-2">About Us</Link>
+                <Link className="text-white mb-1 py-2">FAQ</Link>
+                <Link className="text-white mb-1 py-2">Contact </Link>
+              </div>
+            </div>
+            <div className="col-2 ">
+              <h4 className="mb-4 text-white">Quick Links</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white mb-1 py-2">Laptops</Link>
+                <Link className="text-white mb-1 py-2">Headphones</Link>
+                <Link className="text-white mb-1 py-2">Tablets</Link>
+                <Link className="text-white mb-1 py-2">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()}; Created By Saumya
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
