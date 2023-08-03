1dc0c7fadb6e6d818dd0ea72d792e77bf3e75c9e
urvashi1210 <urvashishukla1210@gmail.com>
2023-08-03 22:02:47 +0530

    data manipulated

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
             <div className="col-5">
               <div className="footer-top-data d-flex gap-30 align-items-center">
                 <img src={newsletter}alt="newsletter" />
-                <h2 className="mb-0 text-white">Sign Up for our Newsletter</h2>
+                <h2 className="mb-0 text-white">Sign Up for Newsletter</h2>
               </div>
             </div>
             <div className="col-7">
