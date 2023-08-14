e8564481c93f7661e48b20e758699045ef7acf3c
urvashi1210 <urvashishukla1210@gmail.com>
2023-08-14 13:40:39 +0530

    Completed wrapper-1(home page)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="container-xxl">
       <div className="row">
         <div className="col-6">
-          <div className="main-banner position-relative p-3">
+          <div className="main-banner position-relative">
             <img src="images/main-banner-1.jpg" className="img-fluid rounded-3" alt="main banner"/>
             <div className="main-banner-content position-absolute">
               <h4>
@@ -19,14 +19,71 @@ const Home = () => {
               <p>
                 From $999.00 or $41.62/mo.
               </p>
-              <Link>
+              <Link className="button">
                 BUY NOW
               </Link>
             </div>
           </div>
         </div>
         <div className="col-6">
-
+        <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+        <div className="small-banner position-relative">
+            <img src="images/catbanner-01.jpg" className="img-fluid rounded-3" alt="main banner"/>
+            <div className="small-banner-content position-absolute">
+              <h4>
+               Best Sale
+              </h4>
+              <h5>
+                iPad S13+ Pro.
+              </h5>
+              <p>
+                From $999.00 <br/> or $41.62/mo.
+              </p>
+            </div>
+          </div>
+          <div className="small-banner position-relative">
+            <img src="images/catbanner-02.jpg" className="img-fluid rounded-3" alt="main banner"/>
+            <div className="small-banner-content position-absolute">
+              <h4>
+                NEW ARRIVAL
+              </h4>
+              <h5>
+                Buy iPad Air
+              </h5>
+              <p>
+                From $999.00 <br/> or $41.62/mo.
+              </p>
+            </div>
+          </div>
+          <div className="small-banner position-relative">
+            <img src="images/catbanner-03.jpg" className="img-fluid rounded-3" alt="main banner"/>
+            <div className="small-banner-content position-absolute">
+              <h4>
+                NEW ARRIVAL
+              </h4>
+              <h5>
+                Buy iPad Air
+              </h5>
+              <p>
+                From $999.00 <br/> or $41.62/mo.
+              </p>
+            </div>
+          </div>
+          <div className="small-banner position-relative p-3">
+            <img src="images/catbanner-04.jpg" className="img-fluid rounded-3" alt="main banner"/>
+            <div className="small-banner-content position-absolute">
+              <h4>
+                NEW ARRIVAL
+              </h4>
+              <h5>
+                Buy iPad Air
+              </h5>
+              <p>
+                From $999.00 <br/> or $41.62/mo.
+              </p>
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </div>
@@ -34,4 +91,4 @@ const Home = () => {
   </>
 }
 
-export default Home
+export default Home;
