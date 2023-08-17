21b4a92733f03bfa4cd462e8067ee5331bcaa27f
urvashi1210 <urvashishukla1210@gmail.com>
2023-08-18 00:38:25 +0530

    Added action bar and ratings section

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ReactStars from "react-rating-stars-component";
+import {Link} from 'react-router-dom';
 
 const ProductCard = () => {
   return (
     <div className="col-3">
-      <div className="product-card">
+      <div className="product-card position-relative">
         <div className="product-image">
         <img src="images/watch.jpg" alt="product img" />
       </div>
@@ -12,7 +14,26 @@ const ProductCard = () => {
         <h5 className="product-title">
           Kids headphones bulk 10 pack multi colored for students
         </h5>
+        <ReactStars
+    count={5}
+    value="3" edit={false}
+    size={24}
+    activeColor="#ffd700"
+  />
         <p className="price">$100.00</p>
+      </div>
+      <div className="action-bar position-absolute">
+        <div className="d-flex flex-column">
+          <Link>
+          <img src="images/prodcompare.svg" alt="compare" />
+          </Link>
+          <Link>
+          <img src="images/view.svg" alt="addCart"/>
+          </Link>
+          <Link>
+          <img src="images/add-cart.svg" alt="addCart" />
+          </Link>
+        </div>
       </div>
     </div>
     </div>
