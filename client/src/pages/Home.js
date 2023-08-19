25a9c488dad2174adf9b54bcaa8731a5c0358ee5
urvashi1210 <urvashishukla1210@gmail.com>
2023-08-19 23:28:11 +0530

    working on special wrapper

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -249,6 +249,22 @@ const Home = () => {
       </div>
     </div>
   </section>
+  <section className="famous-wrapper py-5 home-wrapper-2">
+   <div className="container-xxl">
+    <div className="row">
+      <div className="col-3">
+        <div className="famous-card bg-dark position-relative">
+          <img src="images/famous-1.webp" alt="famous" />
+          <div className="famous-content position-absolute">
+          <h5>Big Screen</h5>
+          <h6>Smart Watch Series 7</h6>
+          <p>From $399 or $16.62/mo. for 24 mo. </p>
+        </div>
+        </div>
+      </div>
+      </div>
+      </div> 
+  </section>
   <div className="special-wrapper py-5 home-wrapper-2">
     <div className="container-xxl">
       <div className="row">
