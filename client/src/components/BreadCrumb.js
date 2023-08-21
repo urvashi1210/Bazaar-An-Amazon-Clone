c88298988c7baa9ee4fc9777947464b0b71c82f0
urvashi1210 <urvashishukla1210@gmail.com>
2023-08-21 23:50:37 +0530

    Styling breadcrumb component

---
+++ client/src/components/BreadCrumb.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const BreadCrumb = (props) => {
+    const {title}=props;
+  return (
+    <div className="breadcrumb py-4">
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-12">
+                    <p>
+                        <Link to="/" className="text-dark">Home{" "}</Link>{" "}/{title}
+                    </p>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+  )
+}
+
+export default BreadCrumb
---
+++ client/src/pages/OurStore.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BreadCrumb from '../components/BreadCrumb'
+
+const OurStore = () => {
+  return (
+    <div>
+      <BreadCrumb title="Our Store"/>
+    </div>
+  )
+}
+
+export default OurStore
