3c1ad26c192afa05a84245646d2bdb3679b68a8b
urvashi1210 <urvashishukla1210@gmail.com>
2023-08-23 23:42:19 +0530

    working

--- client/src/components/BreadCrumb.js
+++ client/src/components/BreadCrumb.js
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom'
 const BreadCrumb = (props) => {
     const {title}=props;
   return (
-    <div className="breadcrumb py-4">
+    <div className="breadcrumb mb-0 py-4">
         <div className="container-xxl">
             <div className="row">
                 <div className="col-12">
-                    <p>
-                        <Link to="/" className="text-dark">Home{" "}</Link>{" "}/{title}
+                    <p className="text-center mb-0">
+                        <Link to="/" className="text-dark">Home</Link>/{title}
                     </p>
                 </div>
             </div>
